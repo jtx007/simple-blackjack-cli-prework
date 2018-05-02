697a8dfd3740dcff420e88a405106992d2d8e379
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-02 02:21:02 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,14 +42,7 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   get_user_input
-  if get_user_input == 'h'
-    card_total += deal_card
-    card_total
-    elsif get_user_input == 's'
-      card_total
-    else 
-     invalid_command
-   end
+  card_total
 
 end
 
