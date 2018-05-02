307dffefa5f26d7eacdcdf8d338ba3dabeebb6e7
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-02 02:03:33 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,13 +37,20 @@ def initial_round
   display_card_total(card_total)
   card_total
 end
-require 'pry'
+
 def hit?(card_total)
   # code hit? here
   prompt_user
   get_user_input
-  binding.pry
-  
+  if get_user_input == 's'
+    card_total
+    elsif get_user_input == 'h'
+    card_total += deal_card
+    card_total
+  else
+    invalid_command
+  end
+
 end
 
 def invalid_command
